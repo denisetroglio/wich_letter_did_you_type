37398ac6fad67ef72c5054d3c4986ba11d458f3c
Denise Troglio <overbeck.denise@gmail.com>
2022-03-14 14:02:27 +0100

    add starter kit

---
+++ docs/assets/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const inputText = document.querySelector('.js_input');
+const numberKey = document.querySelector('.js_nk');
+const key = document.querySelector('.js_k');
+const code = document.querySelector('.js_c');
+
+function handleWrite(event) {
+  numberKey.innerHTML = event.keyCode;
+  key.innerHTML = event.key;
+  code.innerHTML = event.code;
+  inputText.value = '';
+}
+
+inputText.addEventListener('keyup', handleWrite);
