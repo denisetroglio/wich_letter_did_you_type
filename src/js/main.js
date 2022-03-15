7766da21a67b028d669b5cb137ed23a10161005f
Denise Troglio <overbeck.denise@gmail.com>
2022-03-15 11:18:12 +0100

    add clean input type mobile in JS

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,14 @@ const inputText = document.querySelector('.js_input');
 const numberKey = document.querySelector('.js_nk');
 const key = document.querySelector('.js_k');
 const code = document.querySelector('.js_c');
+const mobile = document.querySelector('.js_mobile');
 
 function handleWrite(event) {
   numberKey.innerHTML = event.keyCode;
   key.innerHTML = event.key;
   code.innerHTML = event.code;
   inputText.value = '';
+  mobile.value = '';
 }
 
 inputText.addEventListener('keyup', handleWrite);
